1d660e0fffef76d298dbef89a08b48667ca54afc
FlavioGomesMenezesPorto <flavioporto203@gmail.com>
2025-09-19 19:41:32 -0300

    Atualizando Financeiro 1.01

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),  # Adiciona rotas de login/logout
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
